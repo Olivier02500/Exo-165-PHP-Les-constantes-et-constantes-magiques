145bfa9b70b44e41fe6ed086bc6de72773126a52
Olivier02500 <olivier.peuchet@hotmail.com>
2022-01-23 13:52:37 +0100

    make exo 165

--- index.php
+++ index.php
@@ -10,8 +10,23 @@
  */
 // TODO Votre code ici.
 
+define('CONSTANTE', 12);
+define('CONSTANTE2', 2);
 
+echo CONSTANTE * CONSTANTE2."<br>";
+
+define('CONSTANTE3', "C'est cool PHP");
+echo (CONSTANTE + CONSTANTE2) * strlen(CONSTANTE3);
 /**
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
+
+echo basename(__FILE__)."<br>";
+echo basename(__DIR__)."<br>";
+echo basename(__LINE__)."<br>";
+echo basename(__FUNCTION__)."<br>";
+echo basename(__CLASS__)."<br>";
+echo basename(__METHOD__)."<br>";
+echo basename(__NAMESPACE__)."<br>";
+echo basename(__TRAIT__)."<br>";
